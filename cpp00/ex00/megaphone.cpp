7260b3841f05d60662c2f688f2e857838ac7bdfd
Yuri Fontenele <yurifontenelesilva180303@gmail.com>
2024-08-27 16:50:44 -0300

    :construction: feat: add modifiers

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: yufonten <yufonten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 15:23:15 by yufonten          #+#    #+#             */
-/*   Updated: 2024/08/27 16:07:02 by yufonten         ###   ########.fr       */
+/*   Updated: 2024/08/27 16:11:44 by yufonten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int	main(int ac, char **av)
 {
-	int		i;
-	int		j;
+	int	i;
+	int	j;
 
 	if (ac == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
