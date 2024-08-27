188f1ad9b942ada622654fa1b07fdfa695de1e15
Yuri Fontenele <yurifontenelesilva180303@gmail.com>
2024-08-27 16:10:33 -0300

    :sparkles: feat: resolve for ex00

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yufonten <yufonten@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/27 15:23:15 by yufonten          #+#    #+#             */
+/*   Updated: 2024/08/27 16:07:02 by yufonten         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int ac, char **av)
+{
+	int		i;
+	int		j;
+
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	i = 1;
+	while (ac > 1 && av && av[i])
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			std::cout << (char)std::toupper(av[i][j]);
+			j++;
+		}
+		i++;
+	}
+	std::cout << std::endl;
+	return (0);
+}
