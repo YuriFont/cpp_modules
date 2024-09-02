22320fc47d393a4e416aa52ac72686e84d30d98c
Yuri Fontenele <yurifontenelesilva180303@gmail.com>
2024-09-02 16:58:29 -0300

    :construction: construction Account.cpp

---
+++ cpp00/ex02/Account.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yufonten <yufonten@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/02 13:47:29 by yufonten          #+#    #+#             */
+/*   Updated: 2024/09/02 16:57:56 by yufonten         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+#include <iostream>
+
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+Account::Account(int initial_deposit) {
+	this->_accountIndex = this->_nbAccounts;
+	this->_totalAmount += initial_deposit;
+	this->_amount = initial_deposit;
+	this->_nbDeposits = 0;
+	
+}
