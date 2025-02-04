6bb3d986b74295aed226fbc217a263787efeb30f
YuriFont <yurifontenelesilva180303@gmail.com>
2025-02-04 10:39:04 -0300

    :sparkles: finish ex02

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -25,6 +25,7 @@ int	main(void) {
 
 	std::cout << std::endl;
 
+	std::cout << "The value of the string variable: " << str << std::endl;
 	std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
 	std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
 
