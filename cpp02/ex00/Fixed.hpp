108794997731ddcf77f142aae2bf4dfe6e734d4b
YuriFont <yurifontenelesilva180303@gmail.com>
2025-02-06 19:19:49 -0300

    :sparkles: initial commit

---
+++ cpp02/ex00/Fixed.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yufonten <yufonten@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/06 11:39:40 by yufonten          #+#    #+#             */
+/*   Updated: 2025/02/06 11:39:40 by yufonten         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef __FIXED_HPP__
+# define __FIXED_HPP__
+# include <iostream>
+
+class Fixed {
+
+    private:
+
+        static const int    fractionalBits;
+        int                 value;
+        
+    public:
+
+        Fixed(void);
+        Fixed(const Fixed& fixed);
+        Fixed& operator=(const Fixed& fixed);
+        ~Fixed(void);  
+        int     getRawBits(void) const;
+        void    setRawBits(int const raw);
+
+
+};
+
+#endif
