2a20d039bf7ba8326a1260415addd8f19ddf0041
YuriFont <yurifontenelesilva180303@gmail.com>
2025-03-05 01:12:18 -0300

    init cpp04

---
+++ cpp04/ex00/Animal.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yufonten <yufonten@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/05 00:12:40 by yufonten          #+#    #+#             */
+/*   Updated: 2025/03/05 00:12:40 by yufonten         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __ANIMAL_HPP__
+#define __ANIMAL_HPP__
+#include <iostream>
+
+class Animal {
+
+    protected:
+
+        std::string type;
+
+    public:
+
+        Animal(void);
+        Animal(const Animal &animal);
+        Animal  &operator=(const Animal &animal);
+        ~Animal(void);
+
+};
+
+#endif
