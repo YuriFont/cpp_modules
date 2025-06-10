3497490a886e0ead9af69f59d3984b6907e93d95
YuriFont <yurifontenelesilva180303@gmail.com>
2025-06-10 09:55:20 -0300

    :sparkles: innitial commits

---
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yufonten <yufonten@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/04 11:00:33 by yufonten          #+#    #+#             */
+/*   Updated: 2025/06/04 11:01:54 by yufonten         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+class BitcoinExchange {
+
+    private:
+
+        
+    
+    public:
+
+        
+    
+};
+
+#endif
